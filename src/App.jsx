578fa2d6096c4074f65d82b793e638e15bc48896
heyam <heyam976@gmail.com>
2022-12-25 21:21:44 +0200

    done issue 2 and 10

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,12 @@ import React from 'react';
 // import {useState} from 'react'
 import {Routes,Route} from "react-router-dom"
 import './App.css';
+import Footer from './Components/home/footer';
 import NavBar from './Components/home/navbar';
 import Home from './Containers/Home';
 // import { Switch, Route } from "react-router-dom";
+
+
 
 
 function App() {
@@ -13,10 +16,13 @@ function App() {
     <div>
       {/* <NavBar onChangePage={setPage}/> */}
       <NavBar/>
+     
         <Routes>
           <Route path="/Home" element={<Home />} />
+
         </Routes>
-            
+        <Footer/>
+        
     </div>
   );
 }
--- src/Containers/Home.js
+++ src/Containers/Home.js
@@ -1,10 +1,14 @@
 import React from "react";
 import Testimonial from "../Components/home/testimonials";
+import HomeBody from "../Components/home/home"
+
 
 function Home(){
     return(
         <div>
+            <HomeBody/>
             <Testimonial/>
+          
         </div>
     )
 }
