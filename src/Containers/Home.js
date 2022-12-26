e389542e8d9bebb1b880a27106385f4f67b0bdea
banelmikhael <banel.mikhael.bm@gmail.com>
2022-12-26 22:13:41 +0300

    Create the about part and features in home Fixes #5 (finished)

--- src/Containers/Home.js
+++ src/Containers/Home.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import Testimonial from '../Components/home/testimonials';
+import About from '../Components/home/about';
+import Features from '../Components/home/features';
 
 function Home() {
   return (
     <div>
       <Testimonial />
+      <About />
+      <Features />
     </div>
   );
 }
