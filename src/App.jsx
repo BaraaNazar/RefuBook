8a9c52e4b8530121bd320559524b131f8abfb48f
Sakar Masoud <masoudsakar@gmail.com>
2022-12-27 16:53:23 +0300

    Sakar

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,7 @@ import {Routes,Route} from "react-router-dom";
 import './App.css';
 import NavBar from './Components/home/navbar';
 import Home from './Containers/Home';
-//import { Switch, Route } from "react-router-dom";
-import LatestStories from './Components/home/stories/index';
+// import { Switch, Route } from "react-router-dom";
 
 
 function App() {
@@ -16,7 +15,7 @@ function App() {
         <Routes>
           <Route path="/Home" element={<Home />} />
         </Routes>
-      <LatestStories/>   
+      <Home />
     </div>
   );
 }
