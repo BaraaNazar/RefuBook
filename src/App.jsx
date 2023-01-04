9099de91f55c4dc4609802a163e7c11ee667d830
Baraa-Nazar <baraa.nazar.ph@gmail.com>
2023-01-04 12:12:03 +0300

    Completed issue 23/filter function for about page

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,19 @@
 import React from 'react';
 // import {useState} from 'react'
-import {Routes,Route} from "react-router-dom";
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import NavBar from './Components/home/navbar';
 import Home from './Containers/Home';
 // import { Switch, Route } from "react-router-dom";
 
-
 function App() {
   return (
     <div>
       {/* <NavBar onChangePage={setPage}/> */}
-      <NavBar/>
-        <Routes>
-          <Route path="/Home" element={<Home />} />
-        </Routes>
+      <NavBar />
+      <Routes>
+        <Route path="/Home" element={<Home />} />
+      </Routes>
       <Home />
     </div>
   );
