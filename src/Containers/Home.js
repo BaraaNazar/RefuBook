3e124c4e0a7e8dcf8f4eb377601cbe581a4d28a6
Baraa-Nazar <baraa.nazar.ph@gmail.com>
2023-01-05 14:47:30 +0300

    Completed Partners and fixing about page due to new added Partners component's content

--- src/Containers/Home.js
+++ src/Containers/Home.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import Testimonial from '../Components/home/testimonials';
+import Partners from '../Components/home/partners';
 import About from '../Components/home/about';
 import Features from '../Components/home/features';
 import LatestStories from '../Components/home/stories';
 import HomeBody from '../Components/home/home';
 
-
 function Home() {
   return (
     <div>
-      <HomeBody/>
+      <HomeBody />
       <About />
       <Features />
       <Testimonial />
+      <Partners />
       <LatestStories />
     </div>
   );
