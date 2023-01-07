6bf6987c6ac7127e0c24f2033d52249b6cf44ed7
Baraa-Nazar <baraa.nazar.ph@gmail.com>
2023-01-07 19:38:53 +0300

    Completed issue of get in touch and linked to the firebase

--- src/Components/contact-page/index.js
+++ src/Components/contact-page/index.js
@@ -1,9 +1,33 @@
 import { ImFacebook, ImTwitter } from 'react-icons/im';
 import { FaInstagram } from 'react-icons/fa';
+import React, { useState } from 'react';
+import { addDoc,collection } from "firebase/firestore";
+import db from '../../Firebase/firebase';
 import NavBar from '../../Containers/Navbar';
 import contactImage from '../../Contact.png';
 
 function Contacts() {
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+
+  async function addingcontact() {
+    await addDoc(collection(db, "Contact"), {
+      email,
+      message
+      
+    }).then(d => {
+      console.log(d);
+    });
+  }
+
+  
+  const saveDoc = (e) => {
+    e.preventDefault();
+
+    addingcontact();
+  };
+
+
   return (
     <div className="h-screen w-full bg-[#70CDD6] flex items-center justify-center">
       <div className="h-[90vh] xl:h-[90vh] w-11/12 xl:w-[90%] py-6 px-4 bg-[#E7FAFF] rounded-6xl shadow-custom-xl relative overflow-hidden">
@@ -17,20 +41,20 @@ function Contacts() {
               But Brooke Chaffin and Catherine Connors are looking to change
               that with the introduction.
             </p>
-            <form className="flex flex-col">
+            <form onSubmit={saveDoc} className="flex flex-col">
               <p className="md:text-[10px] text-[#8B8F9C]  md:pt-3">Email</p>
-              <input
+              <input value={email} onChange={(e) => setEmail(e.target.value)}
                 placeholder="hello@gmail.com"
                 className="md:w-[450px] h-[50px] sm:h-[50px] rounded-md text-left "
               />
-              <textarea
+              <textarea value={message} onChange={(e) => setMessage(e.target.value)}
                 type="text"
                 placeholder="Your Message"
                 aria-label="disabled input"
                 className="rounded-md block min-h-[160px] max-h-[160px] md:min-h-[199px]  md:max-h-[200px] md:w-[450px] mt-3"
               />
               <button
-                type="button"
+                type="submit"
                 className="bg-[#4699C2] mx-auto rounded-[1009px] mt-1 md:ml-3 md:mt-1 text-white w-[125px] h-[40px]"
               >
                 Send
