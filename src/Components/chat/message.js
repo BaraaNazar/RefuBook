4516f9d90e38aaa0f062d1473c8df68da15ab5cf
Baraa-Nazar <baraa.nazar.ph@gmail.com>
2023-01-15 19:22:39 +0300

    fixed deployment

--- src/Components/chat/message.js
+++ src/Components/chat/message.js
@@ -1,27 +1,27 @@
-import React, { useState } from "react";
-import {onAuthStateChanged} from "firebase/auth"
-import {auth} from "../../Firebase/firebase"
+import React, { useState } from 'react';
+import { onAuthStateChanged } from 'firebase/auth';
+import { auth } from '../../Firebase/firebase';
 
-function Message({message}){
-    const [user, setUser]= useState({})
+function Message({ message }) {
+  const [user, setUser] = useState({});
 
-    onAuthStateChanged(auth,(currentUser)=>{
-        if(currentUser){
-            setUser(currentUser)
-        }
-    })
-    const profilePicture = user.photoURL
-    return(
-<div className="col-start-6 col-end-13 p-3 rounded-lg relative">
+  onAuthStateChanged(auth, (currentUser) => {
+    if (currentUser) {
+      setUser(currentUser);
+    }
+  });
+  const profilePicture = user.photoURL;
+  return (
+    <div className="col-start-6 col-end-13 p-3 rounded-lg relative">
       <div className="flex items-center justify-start flex-row-reverse">
         <div className="flex items-center justify-center h-10 w-10 rounded-full bg-indigo-500 flex-shrink-0">
-        <img className="rounded-full" src={profilePicture} alt="emailPhoto"/>
+          <img className="rounded-full" src={profilePicture} alt="emailPhoto" />
         </div>
         <div className="relative mr-3 text-sm bg-indigo-100 py-2 px-4 shadow rounded-xl">
-            <div>{message.text}</div>
+          <div>{message.text}</div>
         </div>
-        </div>
-        </div>
-    )
+      </div>
+    </div>
+  );
 }
 export default Message;
--- src/Components/chat/user.js
+++ src/Components/chat/user.js
@@ -1,35 +1,27 @@
+import React, { useState } from 'react';
+import { onAuthStateChanged } from 'firebase/auth';
+import { auth } from '../../Firebase/firebase';
+import Toggle from './toggleBtn';
 
+function User() {
+  const [user, setUser] = useState({});
 
-import React, { useState } from "react";
-import {onAuthStateChanged} from "firebase/auth"
-import {auth} from "../../Firebase/firebase"
-import Toggle from "./toggleBtn"
-
-function User(){
-    const [user, setUser]= useState({})
-
-    onAuthStateChanged(auth,(currentUser)=>{
-        if(currentUser){
-            setUser(currentUser)
-        }
-    })
-    const profilePicture = user.photoURL
-    const userName = user.displayName
-return (
-    <div
-    className="flex flex-col items-center bg-indigo-100 border border-gray-200 mt-4 w-full py-6 px-4 rounded-lg"
-    >
-    <div className="h-20 w-20 rounded-full border overflow-hidden">
-        <img
-        src={profilePicture}
-        alt="Avatar"
-        className="h-full w-full"
-        />
-    </div>
-    <div className="text-sm font-semibold mt-2">{userName}</div>
-    <div className="text-xs text-gray-500">Front-end Developer</div>
-    <div className="flex flex-row items-center mt-3">
-        <Toggle/>
+  onAuthStateChanged(auth, (currentUser) => {
+    if (currentUser) {
+      setUser(currentUser);
+    }
+  });
+  const profilePicture = user.photoURL;
+  const userName = user.displayName;
+  return (
+    <div className="flex flex-col items-center bg-indigo-100 border border-gray-200 mt-4 w-full py-6 px-4 rounded-lg">
+      <div className="h-20 w-20 rounded-full border overflow-hidden">
+        <img src={profilePicture} alt="Avatar" className="h-full w-full" />
+      </div>
+      <div className="text-sm font-semibold mt-2">{userName}</div>
+      <div className="text-xs text-gray-500">Front-end Developer</div>
+      <div className="flex flex-row items-center mt-3">
+        <Toggle />
 
         <div className="leading-none ml-1 font-bold text-xs">Active</div>
       </div>
