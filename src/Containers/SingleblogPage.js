1535e0c5c1c2b75ae8f0112a08756debba8672c9
heyam <heyam976@gmail.com>
2023-01-18 20:01:33 +0200

    Blog page and single blog

---
+++ src/Containers/SingleblogPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Singleblog from '../Components/blog-page/single blog page';
+
+function SingleblogPage(){
+    return(
+        <>
+        <Singleblog/>
+        </>
+    )
+
+
+}
+
+export default SingleblogPage;
