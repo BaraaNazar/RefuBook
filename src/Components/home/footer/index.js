57ce319f6a227bc0ca84ca2ccc58b4ae83ecb4eb
heyam <heyam976@gmail.com>
2023-01-21 22:22:48 +0200

    fix footer responsive

--- src/Components/home/footer/index.js
+++ src/Components/home/footer/index.js
@@ -16,8 +16,8 @@ function Footer() {
   }, []);
 
   return (
-    <footer className="flex flex-col md:flex-row items-center justify-center md:justify-between flex-wrap bg-white p-2 lg:px-16">
-      <div className="mx-auto xl:pr-8 mt-5 md:mt-1 mb-5 md:mb-1 flex justify-center items-center flex-shrink-0 text-white justify-center">
+    <footer className="lg:flex lg:justify-between  lg:py-9 lg:mx-24 max-lg:flex max-lg:flex-col max-lg:items-center max-lg:justify-center">
+      <div className=" items-center lg:flex lg:flex-row  max-lg:flex max-lg:flex-col max-lg:items-center max-lg:justify-center ">
         <NavLink to="/" className="flex  md:block md:w-auto tracking-tight ">
           <img src={Logo} className="h-6 mr-3 sm:h-9 " alt="Flowbite Logo" />
         </NavLink>
@@ -58,28 +58,21 @@ function Footer() {
               Contact
             </NavLink>
           </li>
-          <li className="mr-6">
-            <NavLink
-              to="/singleblog"
-              className="text-base font-normal hover:underline block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-            >
-              Single Blog
-            </NavLink>
-          </li>
+    
         </ul>
       </div>
 
-      <div className="mx-auto mt-10 md:mt-1 px-16 flex lg:justify-center">
+      <div className="lg:flex lg:flex-row  max-lg:pb-4 max-lg:flex  max-lg:items-center max-lg:justify-center">
         <div>
           <div className="flex justify-between">
-            <div className="flex justify-between items-center mr-3 ml-3">
-              <ul className="flex  items-center  p-4 mt-4  md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0  dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+            <div className=" items-center lg:flex lg:flex-row    max-lg:flex  max-lg:items-center max-lg:justify-center">
+              <ul className="flex  items-center p-4 mt-4 space-x-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0  dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
                 <li>
                   {!user.displayName ? (
                     <NavLink to="/login">
                       <button
                         type="button"
-                        className="bg-sky-600 hover:bg-sky-400 text-white font-bold py-1 px-6  rounded-full "
+                        className="bg-sky-600 hover:bg-sky-400 text-white font-bold py-1 px-6  rounded-full  "
                         href="/signUp"
                       >
                         Sign up
