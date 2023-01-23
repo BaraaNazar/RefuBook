ec9b245bdfe4cae21b402bf8c1257c39c389bac4
Baraa-Nazar <baraa.nazar.ph@gmail.com>
2023-01-23 21:49:23 +0300

    solved our story bug

--- src/Components/About/story/index.js
+++ src/Components/About/story/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ReadMoreReadLess from './ReadMore-ReadLess';
 
 function Story() {
   return (
@@ -7,7 +8,9 @@ function Story() {
         <h1 className="md:text-6xl text-3xl font-bold capitalize text-blue-800">
           Our Story
         </h1>
-        <p className="text-left">{`
+        <div className="text-left">
+          <ReadMoreReadLess>
+          {`
           In war-torn Syria, the life of a young boy named Ahmad was turned upside down. His home was destroyed, and he was forced to flee with his family for his life. After a perilous journey, they finally reached a refugee camp in Lebanon. Life in the camp was difficult, with limited resources and little hope for the future.
 
           But Ahmad never gave up hope. He knew that he needed to keep moving forward, to find a safe place to rebuild his life. He spent his days in the camp helping his family and learning new skills, such as learning English and fixing bikes.
@@ -21,7 +24,9 @@ function Story() {
           Ahmad's story is just one of the many stories of Syrian refugees who have had to leave their homes in search of safety. They remind us of the resilience and determination of those who have been forced to flee their homes. With your help, we can continue to support Syrian refugees like Ahmad and give them the chance for a new beginning.
           
           Call to Action: Learn more about the Syrian refugee crisis and how you can help. Visit our website and share help to support Syrian refugee families like Ahmad's.
-        `}</p>
+        `}
+        </ReadMoreReadLess>
+        </div>
       </div>
     </div>
   );
